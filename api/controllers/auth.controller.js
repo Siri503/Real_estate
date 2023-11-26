efe9d029d1678b65a104acecdb445cd15b64fa47
Siri503 <sirikanthiachanta@gmail.com>
2023-11-26 17:37:28 +0530

    middleware,error handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcrpytjs from 'bcryptjs';
-const signup=async(req,res)=>{
+const signup=async(req,res,next)=>{
    // console.log(req.body);
    const {username,email,password}=req.body;
    const hashedPassword=bcrpytjs.hashSync(password,10);
@@ -9,7 +9,8 @@ const signup=async(req,res)=>{
    await newUser.save()
    res.status(201).json("User created successfully");
   }catch(error){
-   res.status(500).json(error.message);
+  //  res.status(500).json(error.message);
+  next(error);
   }
 }
 // try{ 
