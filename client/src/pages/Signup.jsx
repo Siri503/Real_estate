2d2de6b7698f23d79cd8e47d7f4bf82e55ddecaa
Siri503 <sirikanthiachanta@gmail.com>
2023-12-05 10:22:14 +0530

    Complete Signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,10 @@
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
 import { useState } from 'react';
 const Signup = () => {
-  const [formData,setFormData]=useState({})
+const [formData,setFormData]=useState({});
+const [error,setError]=useState(null);
+const[loading,setLoading]=useState(false);
+const navigate=useNavigate();
 const handleChange=(e)=>{
  setFormData({
   ...formData,
@@ -10,6 +13,8 @@ const handleChange=(e)=>{
 }
 const handleSubmit=async(e)=>{
   e.preventDefault();
+  try{
+  setLoading(true);
   const res=await fetch("/api/auth/signup",
   {
     method:'POST',
@@ -21,8 +26,21 @@ const handleSubmit=async(e)=>{
   );
   const data=await res.json();
   console.log(data);
+   if(data.success===false){
+    setLoading(false);
+    setError(data.message);
+    return;
+   }
+   setLoading(false);
+   setError(null);
+   navigate('/sign-in');
+  }
+  catch(error){
+    setLoading(false)
+    setError(error.message);
+  }
 };
- console.log(formData);
+//  console.log(formData);
  
 return(
   <div className='p-3 max-w-lg mx-auto'>
@@ -34,9 +52,10 @@ return(
       className='border p-3 rounded-lg'id='username'onChange={handleChange}/>
       <input type="text"placeholder='email'
       className='border p-3 rounded-lg'id='email'onChange={handleChange}/>
-      <input type="text"placeholder='password'
+      <input type="password"placeholder='password'
       className='border p-3 rounded-lg'id='password'onChange={handleChange}/>
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      <button disabled={loading}className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        {loading?'Loading..':'Sign Up'}</button>
 
     </form>
     <div className='flex gap-2 mt-5'>
@@ -45,8 +64,8 @@ return(
         <span className='text-blue-700'>Sign in</span>
       </Link>
     </div>
+    {error && <p className='text-red-500 mt-5'>{error}</p>}
   </div>
 );
 };
 export default Signup;
-
