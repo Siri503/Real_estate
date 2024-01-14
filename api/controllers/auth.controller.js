00da1774d1a10f7fe54844f8a88e4e14aa1eb4af
Siri503 <sirikanthiachanta@gmail.com>
2024-01-14 22:08:25 +0530

    complete signin page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,21 +15,25 @@ export const signup=async(req,res,next)=>{
 }
 export const signin=async(req,res,next)=>{
    const {email,password}=req.body;
-   try{
+   try
+   {
       const validUser=await User.findOne({email});
       if(!validUser) return next(errorHandler(404,'User not found!'));
       const validPassword=bcrpytjs.compareSync(password,validUser.password);
       if(!validPassword)
       return next(errorHandler(401,'Wrong credential!'));
+      // console.log(validUser._id)
       const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+      // console.log(token)
       const {password:pass,...rest}=validUser._doc;
       res
-         .cookie('action_token',token,{httpOnly:true})
+         .cookie('access_token',token,{httpOnly:true})
          .status(200)
          .json(rest);
-      }catch(error){
-    next(error);
+      }
+      catch(error){
+        next(error);
+      }
    }
-}
 
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -54,7 +54,7 @@ return(
       className='border p-3 rounded-lg'id='email'onChange={handleChange}/>
       <input type="password"placeholder='password'
       className='border p-3 rounded-lg'id='password'onChange={handleChange}/>
-      <button disabled={loading}className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+      <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
         {loading?'Loading..':'Sign Up'}</button>
 
     </form>
