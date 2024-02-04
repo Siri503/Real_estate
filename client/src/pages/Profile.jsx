345083626061e4423de97c2925369ea178c5b822
Siri503 <sirikanthiachanta@gmail.com>
2024-02-05 00:30:38 +0530

    delete user

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -127,6 +127,22 @@ const handleShowListings=async()=>{
       showListingsError(true);
     }
 }
+const handleListingDelete=async(listingId)=>{
+  try{
+     const res=await fetch(`/api/listing/delete/${listingId}`,{
+       method:'DELETE',
+     });
+     const data=await res.json();
+     if(data.success===false){
+      console.log(data.message);
+      return;
+     }
+     setUserListings((prev)=>prev.filter((listing)=>listing._id!==listingId));
+  }
+  catch(error){
+    console.log(error.message);
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto '>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -179,10 +195,10 @@ const handleShowListings=async()=>{
          <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
           <p >{listing.name}</p>
          </Link>
-         <div className='flex flex-col item-center'>
-          <button className='text-red-700 uppercase'>Delete</button>
+        <div className='flex flex-col item-center'>
+          <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
           <button className='text-green-700 uppercase'>EDIT</button>
-        </div>  
+         </div>  
       </div>)
       }
       </div>
