4ccdc9775002b0b9aa9cba39c021692997b735dd
Siri503 <sirikanthiachanta@gmail.com>
2024-02-11 12:59:46 +0530

    Search.jsx ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute.jsx'
 import CreateListing from './pages/CreateListing.jsx'
 import UpdateListing from './pages/UpdateListing.jsx'
 import { Listing } from './pages/Listing.jsx'
+import Search from './pages/Search.jsx'
 export default function App() {
   return <BrowserRouter>
   <Header/>
@@ -18,6 +19,7 @@ export default function App() {
     <Route path="/sign-in"element={<SignIn/>}/>
     <Route path="/sign-up"element={<SignUp/>}/>
     <Route path="/about"element={<About/>}/>
+    <Route path="/search"element={<Search/>}/>
     <Route path="/listing/:listingId"element={<Listing/>}/>
     <Route element={<PrivateRoute/>}>
         <Route path="/profile"element={<Profile/>}/>
